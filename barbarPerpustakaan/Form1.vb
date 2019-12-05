d2b20aeff6fc3c5d512998d3be3a910900948727
DEP <diazerlanggaputra@ymail.com>
2019-12-05 19:28:40 +0700

    Our project

--- barbarPerpustakaan/Form1.vb
+++
@@ -1,9 +0,0 @@
-<<<<<<< HEAD
-﻿Public Class Form1
-
-End Class
-=======
-﻿Public Class Form1
-
-End Class
->>>>>>> e8892ff6813ce4c5c8a6bdbb09ea585ebe498bb5
--- perpusBarbar/Form1.Designer.vb
+++ perpusBarbar/Form1.Designer.vb
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
 Partial Class Form1
     Inherits System.Windows.Forms.Form
@@ -29,7 +28,7 @@ Partial Class Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(308, 261)
+        Me.ClientSize = New System.Drawing.Size(295, 261)
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
@@ -37,35 +36,3 @@ Partial Class Form1
     End Sub
 
 End Class
-=======
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class Form1
-    Inherits System.Windows.Forms.Form
-
-    'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
-    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
-        Try
-            If disposing AndAlso components IsNot Nothing Then
-                components.Dispose()
-            End If
-        Finally
-            MyBase.Dispose(disposing)
-        End Try
-    End Sub
-
-    'Required by the Windows Form Designer
-    Private components As System.ComponentModel.IContainer
-
-    'NOTE: The following procedure is required by the Windows Form Designer
-    'It can be modified using the Windows Form Designer.  
-    'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
-    Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Text = "Form1"
-    End Sub
-
-End Class
->>>>>>> e8892ff6813ce4c5c8a6bdbb09ea585ebe498bb5
